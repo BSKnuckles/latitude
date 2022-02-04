f20dc5415cb82050bbcbdebb010dff9eb56546df
Michael McNees <mcnees.michael@gmail.com>
2022-02-04 13:31:54 -0500

    Changed error banner formatting

--- components/error-banner.js
+++ components/error-banner.js
@@ -3,17 +3,13 @@ import { ExclamationIcon } from '@heroicons/react/solid'
 export default function ErrorBanner({ title, message }) {
   return (
     <div className='rounded-md bg-yellow-50 p-4'>
-      <div className='flex'>
-        <div className='flex-shrink-0'>
-          <ExclamationIcon className='h-5 w-5 text-yellow-400' aria-hidden='true' />
-        </div>
-        <div className='ml-3'>
-          <h3 className='text-sm font-medium text-yellow-800'>{title}</h3>
-          <div className='mt-2 text-sm text-yellow-700'>
-            <p>{message}</p>
-          </div>
-        </div>
+      <div className='flex items-center'>
+				<ExclamationIcon className='h-5 w-5 text-yellow-400' aria-hidden='true' />
+				<h3 className='ml-3 text-sm font-medium text-yellow-800'>{title}</h3>
       </div>
+			<div className='mt-2 text-sm text-yellow-700'>
+				<p>{message}</p>
+			</div>
     </div>
   )
 }
