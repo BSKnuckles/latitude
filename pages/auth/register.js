5695475eadee8beace4aa91d65e236823e985cf9
Michael McNees <mcnees.michael@gmail.com>
2022-02-04 13:33:08 -0500

    Added registration endpoint and page

--- pages/auth/register.js
+++ pages/auth/register.js
@@ -16,26 +16,31 @@ export default function Register({ csrfToken }) {
   const [error, setError] = useState()
   const router = useRouter()
 
-  const handleMagicLink = async event => {
+  const handleSubmit = async event => {
     event.preventDefault()
-    const { error, status } = await signIn('email', {
-      redirect: false,
-      callbackUrl: 'http://localhost:3000/dashboard',
-      email: event.target[0].value
-    })
-    if (error) setError(error)
-    else router.push('/auth/confirmation')
-  }
-  const handleCredentials = async event => {
-    event.preventDefault()
-    const { error, status } = await signIn('credentials', {
-      redirect: false,
-      callbackUrl: 'http://localhost:3000/dashboard',
-      email: event.target[0].value,
-      password: event.target[1].value
-    })
-    if (error) setError(error)
-    else router.push('/dashboard')
+		const name = event.target[0].value
+		const email = event.target[1].value
+		const password = event.target[2].value
+		const confirm = event.target[3].value
+
+		if (password === confirm) {
+			const { error, message } = await fetch('/api/auth/register', {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json'
+				},
+				body: JSON.stringify({
+					name: name,
+					email: email,
+					password: password
+				})
+			}).then(res => res.json())
+			if (error) setError(error)
+			if (message === 'Successfully created user account!') router.push('/auth/login')
+		}
+		else {
+			setError('Passwords must match!')
+		}
   }
 
   return (
@@ -49,118 +54,92 @@ export default function Register({ csrfToken }) {
                 src='https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg'
                 alt='Workflow'
               />
-              <h2 className='mt-6 text-3xl font-extrabold text-gray-900'>Sign in to your account</h2>
+              <h2 className='mt-6 text-3xl font-extrabold text-gray-900'>Create Your Account</h2>
             </div>
 
             {error && (
               <div className='mt-8'>
-                <ErrorBanner title='Authentication Failed' message={error} />
+                <ErrorBanner title='Registration Failed' message={error} />
               </div>
             )}
 
             <div className='mt-6'>
-              <div>
+              <form onSubmit={handleSubmit} id='credentials-form' className='space-y-6'>
                 <div>
-                  <form onSubmit={handleMagicLink} id='magic-link-form' className='space-y-6'>
-                    <div>
-                      <label htmlFor='email-link' className='block text-sm font-medium text-gray-700'>
-                        Email address
-                      </label>
-                      <div className='mt-1'>
-                        <input
-                          id='email-link'
-                          name='email-link'
-                          type='email'
-                          autoComplete='email'
-                          required
-                          className='appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm'
-                        />
-                      </div>
-                    </div>
-
-                    <div>
-                      <button
-                        type='submit'
-                        className='w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'>
-                        Send Magic Link to Your Inbox
-                      </button>
-                    </div>
-                  </form>
-                </div>
-
-                <div className='mt-6 relative'>
-                  <div className='absolute inset-0 flex items-center' aria-hidden='true'>
-                    <div className='w-full border-t border-gray-300' />
-                  </div>
-                  <div className='relative flex justify-center text-sm'>
-                    <span className='px-2 bg-white text-gray-500'>Or continue with</span>
+                  <label htmlFor='name' className='block text-sm font-medium text-gray-700'>
+                    Name
+                  </label>
+                  <div className='mt-1'>
+                    <input
+                      id='name'
+                      name='name'
+                      type='text'
+                      autoComplete='name'
+                      required
+                      className='appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm'
+                    />
                   </div>
                 </div>
-              </div>
 
-              <div className='mt-6'>
-                <form onSubmit={handleCredentials} id='credentials-form' className='space-y-6'>
-                  <div>
-                    <label htmlFor='email' className='block text-sm font-medium text-gray-700'>
-                      Email address
-                    </label>
-                    <div className='mt-1'>
-                      <input
-                        id='email'
-                        name='email'
-                        type='email'
-                        autoComplete='email'
-                        required
-                        className='appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm'
-                      />
-                    </div>
+                <div>
+                  <label htmlFor='email' className='block text-sm font-medium text-gray-700'>
+                    Email address
+                  </label>
+                  <div className='mt-1'>
+                    <input
+                      id='email'
+                      name='email'
+                      type='email'
+                      autoComplete='email'
+                      required
+                      className='appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm'
+                    />
                   </div>
+                </div>
 
-                  <div className='space-y-1'>
-                    <label htmlFor='password' className='block text-sm font-medium text-gray-700'>
-                      Password
-                    </label>
-                    <div className='mt-1'>
-                      <input
-                        id='password'
-                        name='password'
-                        type='password'
-                        autoComplete='current-password'
-                        required
-                        className='appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm'
-                      />
-                    </div>
-                  </div>
+								<div className='space-y-2'>
+									<div className='space-y-1'>
+										<label htmlFor='password' className='block text-sm font-medium text-gray-700'>
+											Password
+										</label>
+										<div className='mt-1'>
+											<input
+												id='password'
+												name='password'
+												type='password'
+												autoComplete='current-password'
+												required
+												className='appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm'
+											/>
+										</div>
+									</div>
 
-                  <div>
-                    <button
-                      type='submit'
-                      className='w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'>
-                      Sign in
-                    </button>
-                  </div>
+									<div className='space-y-1'>
+										<label htmlFor='confirm-password' className='block text-sm font-medium text-gray-700'>
+											Confirm Password
+										</label>
+										<div className='mt-1'>
+											<input
+												id='confirm-password'
+												name='confirm-password'
+												type='password'
+												autoComplete='current-password'
+												placeholder='Confirm password'
+												required
+												className='appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm'
+											/>
+										</div>
+									</div>
+								</div>
 
-                  <div className='flex items-center justify-center'>
-                    {/* <div className='flex items-center'>
-                      <input
-                        id='remember-me'
-                        name='remember-me'
-                        type='checkbox'
-                        className='h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded'
-                      />
-                      <label htmlFor='remember-me' className='ml-2 block text-sm text-gray-900'>
-                        Remember me
-                      </label>
-                    </div> */}
-
-                    <div className='text-sm'>
-                      <a href='#' className='font-medium text-indigo-600 hover:text-indigo-500'>
-                        Forgot your password?
-                      </a>
-                    </div>
-                  </div>
-                </form>
-              </div>
+                <div>
+                  <button
+                    type='submit'
+                    className='w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'>
+                    Sign Up
+                  </button>
+                </div>
+              </form>
             </div>
           </div>
         </div>
