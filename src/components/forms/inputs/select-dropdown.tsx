79b80e3c113b1d1653e34ce77e57cc91cef2c044
Michael McNees <mcnees.michael@gmail.com>
2022-02-07 20:47:30 -0500

    Created dropdown menu component

---
+++ src/components/forms/inputs/select-dropdown.tsx
@@ -0,0 +1,28 @@
+export default function SelectDropdown({
+	label,
+	options,
+	value,
+	handleChange,
+}) {
+	return (
+		<div>
+			<label
+				htmlFor="location"
+				className="block text-sm font-medium text-gray-700"
+			>
+				{label}
+			</label>
+			<select
+				id="location"
+				name="location"
+				className="mt-1 block w-full rounded-md border-gray-300 py-2 pl-3 pr-10 text-base focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+				defaultValue={value}
+				onChange={handleChange}
+			>
+				{options.map((option) => (
+					<option>{option}</option>
+				))}
+			</select>
+		</div>
+	)
+}
